4e062b60b156479a07f82d03313d3f07141b9bbf
Godley <charlotte.godley@hotmail.co.uk>
2015-10-02 16:39:27 +0100

    Created brightness block, issue #13

--- blockly/blocks/pearl.js
+++ blockly/blocks/pearl.js
@@ -45,3 +45,20 @@ Blockly.Blocks['set_brightness'] = {
     this.setNextStatement(true, null);
   }
 };
+
+Blockly.Blocks['set_pixel_text_colour'] = {
+  helpUrl: 'http://github.com/wearableproject',
+  init: function() {
+    var value = new Blockly.FieldTextInput('0');
+    var text_colours = new Blockly.FieldDropdown([['off',[0,0,0]],['red',[255,0,0]], ['green',[0,255,0]],
+                                                  ['blue',[0,0,255]], ['yellow',[255,255,0]],
+                                                  ['pink',[255,182,193]], ['orange',[255,165,0]],
+                                                  ['purple',[255,0,255]]]);
+    this.setColour(190);
+    this.appendDummyInput().appendField('Set pixel')
+    .appendField(new Blockly.FieldTextInput('1'), 'PIXEL')
+    .appendField('to colour').appendField(text_colours, 'COLOUR');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+  }
+};
